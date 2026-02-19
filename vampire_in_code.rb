bdba155744859eb6851312acdf10b2d2f0a1d651
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-02-19 14:26:24 -0500

    Create vampire_in_code.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ vampire_in_code.rb
@@ -0,0 +1,38 @@
+# vampire
+#   * gives wife infinite probability ( anything over one
+#   * quietly pretends she doesnt exist ( suggesting localized variable )
+#  * never says goodbye ( so no return statement )
+#  * avoids wet places like lakes ( decomposition process excellerated in the damp )
+#  * ladies deny romantic involvement
+
+
+def vampire_castle(a)
+  a
+end
+
+def woman_leaving(a)
+  a
+end
+
+def daffie_girl(a)
+  a
+end
+
+def avoids_water(a)
+  a
+end
+
+def vampire(a, b, c, d)
+  probability = a * 100
+
+  water_avoidance = avoids_water(b)
+
+  banished = woman_leaving(c)
+
+  is_daffie = daffie_girl(d)
+
+  return a
+  return b
+  return c
+  return d
+end
