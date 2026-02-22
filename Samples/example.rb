f95b776954fe071ebc886c430593796c4771b45b
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-02-21 19:31:10 -0500

    Update example.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- Samples/example.rb
+++ Samples/example.rb
@@ -88,7 +88,7 @@ def self.matawa_float(already_done)
 end
 
 def simulute_exchang(a1, a2, b1, b2, c1, c2,
-                     d1, d2, e1, e1, f1, f2,
+                     d1, d2, e1, e2, f1, f2,
                      g1, g2, h1, h2, i1, i2)
     
   a = a1, a2
